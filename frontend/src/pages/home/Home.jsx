1adfe956700f852f175c8a96532e595a090d374b
Dhiraj2006-cmyk <maned8005@gmail.com>
2026-08-18 12:46:07 +0530

    Add internship management system frontend

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../../components/home/Hero.jsx'
 import Objective from '../../components/home/Objective.jsx'
-
+import User from '../../components/home/User.jsx'
 
 function Home() {
   return (
